6ad959222d681468bc7e9b15b5c442ad5f3d0e40
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-06 11:29:25 +0300

    adding the email sending into the job

--- app/controllers/purchase_assets_controller.rb
+++ app/controllers/purchase_assets_controller.rb
@@ -1,8 +1,6 @@
 class PurchaseAssetsController < ApplicationController
   def create
-    VeryLongTaskJob.perform_now
-    PurchaseAssetMailer.send_purchase_confirmation from: params[:owner], to: params[:buyer]
-
+    VeryLongTaskJob.perform_now from: params[:owner], to: params[:buyer]
     redirect_to root_path, notice: "You Have purchased this asset successfully"
   end
 end
--- app/jobs/very_long_task_job.rb
+++ app/jobs/very_long_task_job.rb
@@ -1,8 +1,9 @@
 class VeryLongTaskJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(from:, to:)
     sleep 60
+    PurchaseAssetMailer.send_purchase_confirmation from:from, to: to
     true # the task was completed correctly
   end
 end
